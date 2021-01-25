dc478d3596d11d8cba678d8ba7be242442179c41
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2021-01-25 17:38:58 -0500

    Added list of files to cache, as well as functions to handle install/activate/fetch from cache. Added icon images to webmanifest.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,73 @@
-console.log("Hi from your service-worker.js file!");
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/manifest.webmanifest",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/db.js"
+];
+
+// INSTALL
+
+self.addEventListener("install", (event) => {
+    // PRE-CACHE ALL STATIC ASSETS
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    // TELL BROWSER TO ACTIVATE SERVICE WORKER WHEN INSTALLATION/CACHE IS COMPLETE
+    self.skipWaiting();
+});
+
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data.", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+
+
+
+// FETCH
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/transaction/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request).then(response => {
+                    // IF STATUS CODE RECEIVED IS 200 (GOOD), GETS CLONED AND STORED IN CACHE
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                }).catch(err => {
+                    // IF FETCH NETWORK REQUEST FAILLED, TRIES CACHE
+                    return cache.match(event.request);
+                });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(event.request).then(response => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
